685ed10cf306f964aae055cc3d1891162e829b58
lucas02-pixel <lucasss02122014>
2025-04-27 09:22:11 -0300

    criando repositorio

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+
+let dia = '25 de dezembro'
+alert ('arroz')
+let natal = prompt ('quando vc acha q vai ser o natal')
+alert (`${natal} esta foi sua resposta sera que ta certa`)
+if (natal == dia) {
+    alert ('vc acertou que ruim na verdade o objetivo era você errar para eu te irritar que ruiiiiim')
+}else {
+    alert (`${natal} na verdade a resposta era ${dia} viu eu sou mais inteligente que você`)
+}
