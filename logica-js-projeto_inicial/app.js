5d67d0584f7e72b832c64203d99087bc76013a94
lucas02-pixel <lucasss02122014>
2025-04-27 09:45:51 -0300

    continuando a criação do jogo

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -7,4 +7,11 @@ if (natal == dia) {
     alert ('vc acertou que ruim na verdade o objetivo era você errar para eu te irritar que ruiiiiim')
 }else {
     alert (`${natal} na verdade a resposta era ${dia} viu eu sou mais inteligente que você`)
+    alert ('ai nossa como você não sabe a data do natal')
+    let sera = prompt ('e nada de falar que errou por gramatica porque mesmo você tiver escrito errado isto tambêm conta como erro')
+    alert ('vou mudar o assunto')
 }
+     alert ('eu acho q você esta feliz de mais vou lançar uma frase desmotivacional')
+    alert ('tudo passa nen que seja pr cima de você')
+    prompt ('esta feliz agora ??????')
+    
